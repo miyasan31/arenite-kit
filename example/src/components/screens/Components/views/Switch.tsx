2fe31d0f250e4a0d1c77e5197284ab904b24f715
miyasan31 <miyasan.dev@gmail.com>
2023-12-12 00:51:15 +0900

    feat: `Switch` component

---
+++ example/src/components/screens/Components/views/Switch.tsx
@@ -0,0 +1,45 @@
+import { createAreniteStyle, HStack, Switch, Text, VStack } from 'arenite-kit';
+import React, { useState } from 'react';
+
+export const SwitchExample = () => {
+  const [isEnabled, setIsEnabled] = useState(true);
+
+  const onPressToggleSwitch = () =>
+    setIsEnabled((previousState) => !previousState);
+
+  return (
+    <VStack gap={12}>
+      <Text style={style.title} color="color1">
+        Switch
+      </Text>
+
+      <HStack gap={16}>
+        <Switch
+          value={isEnabled}
+          onValueChange={onPressToggleSwitch}
+          trackColor={'primary'}
+        />
+        <Switch
+          value={isEnabled}
+          onValueChange={onPressToggleSwitch}
+          trackColor={'secondary'}
+        />
+        <Switch
+          value={isEnabled}
+          onValueChange={onPressToggleSwitch}
+          trackColor={'tertiary'}
+        />
+      </HStack>
+    </VStack>
+  );
+};
+
+const style = createAreniteStyle({
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  subtitle: {
+    fontSize: 16,
+  },
+});
