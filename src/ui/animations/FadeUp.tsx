d311c9075e55a1b537ade0ef2f3653d1769d46bd
miyasan31 <miyasan.dev@gmail.com>
2023-12-12 01:16:29 +0900

    fix: type import

--- src/ui/animations/FadeUp.tsx
+++ src/ui/animations/FadeUp.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import type { ReactNode } from 'react';
 import Animated, { FadeInUp, FadeOutUp } from 'react-native-reanimated';
 
 type FadeUpProps = {
--- src/ui/elements/Callout.tsx
+++ src/ui/elements/Callout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import type { ReactNode } from 'react';
 import type { BgThemeProps } from '../../core';
 import { createAreniteStyle } from '../../style';
 import { Text } from '../primitives';
